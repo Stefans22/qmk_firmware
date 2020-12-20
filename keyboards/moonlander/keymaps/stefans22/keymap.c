5933295465aaa7c27245acc62331762740f017fe
Stefan Schultheiß <mail@stefanschultheiss.de>
2020-12-20 21:10:48 +0100

    Added tap/hold for german umlauts and ß
    Breaks the normal repeat character function, but this way we dont need
    extra keys for ä, ö, ü, Ä, Ö, Ü or ß.

--- keyboards/moonlander/keymaps/stefans22/keymap.c
+++ keyboards/moonlander/keymaps/stefans22/keymap.c
@@ -34,6 +34,13 @@ enum layers {
 enum custom_keycodes {
     VRSN = ML_SAFE_RANGE,
     RGB_RST,
+    MY_AE,
+    MY_OE,
+    MY_UE,
+    MY_SS,
+    MY_SAE,
+    MY_SOE,
+    MY_SUE,
     MY_00,
 };
 
@@ -41,8 +48,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
         DE_CIRC, DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    TO(GAME),        TG(NUM), DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,
-        DE_LBRC, DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    KC_NO,           KC_NO,   DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    KC_HOME,
-        DE_RBRC, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_GRV,          DE_BSLS, DE_H,    DE_J,    DE_K,    DE_L,    DE_SCLN, KC_PGUP,
+        DE_LBRC, DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    KC_NO,           KC_NO,   DE_Z,    MY_UE,   DE_I,    MY_OE,   DE_P,    KC_HOME,
+        DE_RBRC, MY_AE,   MY_SS,   DE_D,    DE_F,    DE_G,    DE_GRV,          DE_BSLS, DE_H,    DE_J,    DE_K,    DE_L,    DE_SCLN, KC_PGUP,
         KC_NO,   DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,                              DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_SLSH, KC_PGDN,
         KC_NO,   KC_ESC,  KC_TAB,  KC_LGUI, MO(SHFT),         KC_ESC,          KC_ENT,           MO(FUN), DE_MINS, DE_QUOT, KC_ENT,  KC_END,
                                             KC_BSPC, KC_LCTL, KC_DEL,          KC_RGUI, KC_LALT, KC_SPC
@@ -50,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SHFT] = LAYOUT_moonlander(
         DE_DEG    ,DE_EXLM   ,DE_DQUO   ,DE_SECT   ,DE_DLR    ,DE_PERC   ,_______   ,            _______   ,DE_AMPR   ,DE_SLSH   ,DE_LPRN   ,DE_RPRN   ,DE_EQL    ,DE_QUES   ,
-	DE_LCBR   ,S(DE_Q)   ,S(DE_W)   ,S(DE_E)   ,S(DE_R)   ,S(DE_T)   ,_______   ,            _______   ,S(DE_Z)   ,S(DE_U)   ,S(DE_I)   ,S(DE_O)   ,S(DE_P)   ,_______   ,
-	DE_RCBR   ,S(DE_A)   ,S(DE_S)   ,S(DE_D)   ,S(DE_F)   ,S(DE_G)   ,DE_TILD   ,            DE_PIPE   ,S(DE_H)   ,S(DE_J)   ,S(DE_K)   ,S(DE_L)   ,DE_COLN   ,_______   ,
+	DE_LCBR   ,S(DE_Q)   ,S(DE_W)   ,S(DE_E)   ,S(DE_R)   ,S(DE_T)   ,_______   ,            _______   ,S(DE_Z)   ,MY_SUE    ,S(DE_I)   ,MY_SOE    ,S(DE_P)   ,_______   ,
+	DE_RCBR   ,MY_SAE    ,S(DE_S)   ,S(DE_D)   ,S(DE_F)   ,S(DE_G)   ,DE_TILD   ,            DE_PIPE   ,S(DE_H)   ,S(DE_J)   ,S(DE_K)   ,S(DE_L)   ,DE_COLN   ,_______   ,
 	_______   ,S(DE_Y)   ,S(DE_X)   ,S(DE_C)   ,S(DE_V)   ,S(DE_B)   ,                                  S(DE_N)   ,S(DE_M)   ,DE_LABK   ,DE_RABK   ,DE_QUES   ,_______   ,
 	_______   ,S(KC_ESC) ,S(KC_TAB) ,S(KC_LGUI),_______   ,           S(KC_ESC) ,            S(KC_ENT) ,           _______   ,DE_UNDS   ,DE_DQUO   ,S(KC_ENT) ,_______   ,
 	                                            KC_DEL    ,S(KC_LCTL),KC_BSPC   ,            S(KC_RGUI),S(KC_LALT),S(KC_SPC)
@@ -97,6 +104,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
+
+typedef struct {
+    uint16_t timer;
+    uint16_t tap_keycode;
+    uint16_t hold_keycode;
+    uint16_t modifier_keycode;
+} tap_hold;
+
+tap_hold th_dia_a = { 0, DE_A, DE_ADIA, 0 };
+tap_hold th_dia_o = { 0, DE_O, DE_ODIA, 0 };
+tap_hold th_dia_u = { 0, DE_U, DE_UDIA, 0 };
+tap_hold th_ss = { 0, DE_S, DE_SS, 0 };
+tap_hold th_dia_sa = { 0, DE_A, DE_ADIA, KC_LSFT };
+tap_hold th_dia_so = { 0, DE_O, DE_ODIA, KC_LSFT };
+tap_hold th_dia_su = { 0, DE_U, DE_UDIA, KC_LSFT };
+
+void tap_or_hold(tap_hold *tapHold);
+
+void tap_or_hold(tap_hold *tapHold) {
+    uint16_t keycode;
+    if (timer_elapsed(tapHold->timer) < 250) {
+        keycode = tapHold->tap_keycode;
+    } else {
+        keycode = tapHold->hold_keycode;
+    }
+    if (tapHold->modifier_keycode > 0) {
+        register_code(tapHold->modifier_keycode);
+    }
+    tap_code(keycode);
+    if (tapHold->modifier_keycode > 0) {
+        unregister_code(tapHold->modifier_keycode);
+    }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
@@ -106,10 +147,56 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	case RGB_RST:
             rgblight_mode(1);
 	    return false;
+	case MY_AE:
+            th_dia_a.timer = timer_read();
+	    return false;
+	case MY_OE:
+            th_dia_o.timer = timer_read();
+	    return false;
+	case MY_UE:
+            th_dia_u.timer = timer_read();
+	    return false;
+	case MY_SS:
+            th_ss.timer = timer_read();
+	    return false;
+	case MY_SAE:
+            th_dia_sa.timer = timer_read();
+	    return false;
+	case MY_SOE:
+            th_dia_so.timer = timer_read();
+	    return false;
+	case MY_SUE:
+            th_dia_su.timer = timer_read();
+	    return false;
 	case MY_00:
 	    SEND_STRING ("00");
 	    return false;
         }
+    } else {
+        switch (keycode) {
+        case MY_AE:
+            tap_or_hold(&th_dia_a);
+            return false;
+        case MY_OE:
+            tap_or_hold(&th_dia_o);
+            return false;
+        case MY_UE:
+            tap_or_hold(&th_dia_u);
+            return false;
+        case MY_SS:
+            tap_or_hold(&th_ss);
+            return false;
+        case MY_SAE:
+            tap_or_hold(&th_dia_sa);
+            return false;
+        case MY_SOE:
+            tap_or_hold(&th_dia_so);
+            return false;
+        case MY_SUE:
+            tap_or_hold(&th_dia_su);
+            return false;
+	}
     }
     return true;
 }
+
